9fc8e012c28d8fa570c2c76eed7ba368b8bcfc70
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2021-09-20 10:53:19 -0500

    Add form handlers and state

--- src/js/react/components/AgePredictor.js
+++ src/js/react/components/AgePredictor.js
@@ -1,25 +1,64 @@
-function AgePredictor() {
-    return (
-        <main>
-            <div className="wrapper">
-                <form>
-                    <div>
-                        <label for="names">
-                            Nombres (Si es más de uno, separalos con comas)
-                        </label>
-                        <input type="text" name="names" />
-                    </div>
-                    <div>
-                        <label for="country">País (En inglés)</label>
-                        <input type="text" name="country" />
-                    </div>
-                    <button className="btn">Calcular edad</button>
-                </form>
-                <hr />
-            </div>
-            <div className="wrapper"></div>
-        </main>
-    );
+class AgePredictor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nameList: [],
+            country: "",
+        };
+
+        this.changeHandler = this.changeHandler.bind(this);
+        this.submitHandler = this.submitHandler.bind(this);
+    }
+
+    changeHandler(event) {
+        let { value, name } = event.target;
+        if (name === "nameList") {
+            value = value.split(", ").map(function (name) {
+                return name.trim();
+            });
+        }
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <main>
+                <div className="wrapper">
+                    <form onSubmit={this.submitHandler}>
+                        <div>
+                            <label for="names">
+                                Nombres (Si es más de uno, separalos con comas)
+                            </label>
+                            <input
+                                onChange={this.changeHandler}
+                                type="text"
+                                name="nameList"
+                                value={this.state.nameList.join(", ")}
+                            />
+                        </div>
+                        <div>
+                            <label for="country">País (En inglés)</label>
+                            <input
+                                onChange={this.changeHandler}
+                                type="text"
+                                name="country"
+                                value={this.state.country}
+                            />
+                        </div>
+                        <button className="btn">Calcular edad</button>
+                    </form>
+                    <hr />
+                </div>
+                <div className="wrapper"></div>
+            </main>
+        );
+    }
 }
 
 export default AgePredictor;

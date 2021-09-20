35e356840eb1911288a4f3e26245f3328862abe8
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2021-09-20 11:28:39 -0500

    Add submit handler... not working fully yet

--- src/js/react/components/AgePredictor.js
+++ src/js/react/components/AgePredictor.js
@@ -1,9 +1,15 @@
+import buildQueryString from "../../helpers/buildQueryString";
+import countries from "../../data/countries";
+import AgeResult from "./AgeResult";
+
 class AgePredictor extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             nameList: [],
             country: "",
+            result: {},
+            isLoading: false,
         };
 
         this.changeHandler = this.changeHandler.bind(this);
@@ -24,6 +30,35 @@ class AgePredictor extends React.Component {
 
     submitHandler(event) {
         event.preventDefault();
+
+        let { state, setState } = this;
+
+        setState({
+            isLoading: true,
+        });
+
+        let country = countries.find(function (item) {
+            return state.country === item.Country;
+        })?.ISOCode;
+
+        let query = buildQueryString({
+            names: state.nameList,
+            country,
+        });
+
+        fetch("https://api.agify.io?" + query)
+            .then(function (response) {
+                if (!response.ok) throw response;
+                return response.json();
+            })
+            .then(function (result) {
+                setState({
+                    result,
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     render() {
@@ -32,22 +67,24 @@ class AgePredictor extends React.Component {
                 <div className="wrapper">
                     <form onSubmit={this.submitHandler}>
                         <div>
-                            <label for="names">
+                            <label htmlFor="names">
                                 Nombres (Si es más de uno, separalos con comas)
                             </label>
                             <input
                                 onChange={this.changeHandler}
                                 type="text"
                                 name="nameList"
+                                id="names"
                                 value={this.state.nameList.join(", ")}
                             />
                         </div>
                         <div>
-                            <label for="country">País (En inglés)</label>
+                            <label htmlFor="country">País (En inglés)</label>
                             <input
                                 onChange={this.changeHandler}
                                 type="text"
                                 name="country"
+                                id="country"
                                 value={this.state.country}
                             />
                         </div>
@@ -55,7 +92,9 @@ class AgePredictor extends React.Component {
                     </form>
                     <hr />
                 </div>
-                <div className="wrapper"></div>
+                <div className="wrapper">
+                    <AgeResult result={this.state.result} />
+                </div>
             </main>
         );
     }
